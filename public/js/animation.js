44e04efe16e52e39d5ead1cb3ba4858b333a4728
SamJian <koko0809@gmail.com>
2025-03-25 13:57:48 +0800

    no message

--- public/js/animation.js
+++ public/js/animation.js
@@ -15,22 +15,6 @@ $('.list li img').click(function () {
 
 $('.popUp button').click(function () {
     $('.popUp').removeClass('active');
-	
-	
-	const html5QrCode = new Html5Qrcode("qr-reader");
-
-	const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-			html5QrCode.stop();
-			alert("scan text :" + decodedText);
-			
-			//$('.popUp_qrcode').removeClass('active');
-	};
-		
-	const config = { fps: 10, qrbox: { width: 500, height: 400 } };
-
-	// If you want to prefer front camera test
-	html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
-		
 		
 	//$('.popUp_qrcode').addClass('active');
 });
