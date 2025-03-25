eddb34d6dff46404501c39167ae2009b00a638f9
SamJian <koko0809@gmail.com>
2025-03-25 14:00:38 +0800

    no message

--- public/js/animation.js
+++ public/js/animation.js
@@ -8,7 +8,16 @@ $('#hamburger-button').click(function () {
 
 //popUp
 $('.list li img').click(function () {
-	
+	const qrCodeSuccessCallback = (decodedText, decodedResult) => {
+			html5QrCode.stop();
+			alert("scan text :" + decodedText);
+			//$("#qr-reader-results").html("scan text :" + decodedText);
+
+		};
+		const config = { fps: 10, qrbox: { width: 500, height: 400 } };
+
+		// If you want to prefer front camera test
+		html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
 	
     $('.popUp').addClass('active');
 });
--- resources/views/level.blade.php
+++ resources/views/level.blade.php
@@ -87,20 +87,12 @@
             document.addEventListener("DOMContentLoaded", fn);
         }
     }
+    const html5QrCode = null;
 
     docReady(function () {
         const html5QrCode = new Html5Qrcode("qr-reader");
 
-		const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-			html5QrCode.stop();
-			alert("scan text :" + decodedText);
-			//$("#qr-reader-results").html("scan text :" + decodedText);
 
-		};
-		const config = { fps: 10, qrbox: { width: 500, height: 400 } };
-
-		// If you want to prefer front camera test
-		html5QrCode.start({ facingMode: "environment" }, config, qrCodeSuccessCallback);
     });
     </script>
 </body>
