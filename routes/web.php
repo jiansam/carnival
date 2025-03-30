6e28b7542fcdf529eea49fc8f270decd83956ce4
SamJian <koko0809@gmail.com>
2025-03-30 11:27:34 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,10 @@ Route::get('qrcode', function(){
     return view("index_qrcode");
 });
 
-Route::post('convert/{type}/{phone}', [PageController::class, "convert"]);
-Route::post('manager/{type}/{phone}', [PageController::class, "managerConvert"]);
+Route::get('convert/{type}/{phone}', [PageController::class, "convert"]);
+Route::get('manager/{type}/{phone}', [PageController::class, "managerConvert"]);
 
-Route::post('toLevel', [PageController::class, "toLevel"]);
+Route::get('toLevel', [PageController::class, "toLevel"]);
 Route::get('manager', [PageController::class, "manager"]);
 
 Route::get('index', [PageController::class, "index"]);
