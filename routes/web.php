fa1e874b8d24945d2af151dec670d169e266c257
SamJian <koko0809@gmail.com>
2025-03-30 11:25:19 +0800

    no message

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,7 @@ Route::post('convert/{type}/{phone}', [PageController::class, "convert"]);
 Route::post('manager/{type}/{phone}', [PageController::class, "managerConvert"]);
 
 Route::post('toLevel', [PageController::class, "toLevel"]);
-Route::post('manager', [PageController::class, "manager"]);
+Route::get('manager', [PageController::class, "manager"]);
 
 Route::get('index', [PageController::class, "index"]);
 Route::get('level', [PageController::class, "level"]);
